c591cfa2d830445ff66c712c50f34dd2a95c1304
bartosz <bartoszkruczek@gmail.com>
2020-11-29 15:37:59 +0100

    2i commit dodane algorytmy Konrada i zmodyfikowanie argumentow funkcji generujacych w klasach PyMazeBFS i PyMazeDfs

--- PyMazeBFS.py
+++ PyMazeBFS.py
@@ -2,105 +2,102 @@ import random
 import pygame
 import time
 
-white = (255,250,250)
-red =  (255,0,0)
 
-visited = []
-queue = []
-x = 1
-y = 1
+def generate(s):
+    white = (255, 250, 250)
+    red = (255, 0, 0)
 
-#Ustawiamy parametry okna dla pygame
-print("Podaj rozmiar labiryntu")
-s = input()
+    visited = []
+    queue = []
+    x = 1
+    y = 1
 
-screen = pygame.display.set_mode((int(s)*int('20'),int(s)*int('20')))
-pygame.display.set_caption("Project Labyrinth")
+    # Ustawiamy parametry okna dla pygame
+    # s = int(input("Podaj rozmiar labiryntu\n"))
 
-#Tworzymy przestrzeń w której ma powstać labirynt
-grid = {(x,y):1 for x in range(1, int(s)+int('1')) for y in range(1, int(s)+int('1'))}
+    if (s % 2 == 0): s += 1
 
-def CheckNeighbours():  #Funkcja sprawdza ile mamy wolnych sąsiednich kratek
-    if (x+2, y) not in visited and (x+2,y) in grid:
-        neighb.append("E")
-        if (x+2, y) not in queue:
-            queue.append((x+2,y))
+    screen = pygame.display.set_mode((s * 20, s * 20))
+    pygame.display.set_caption("Project Labyrinth")
 
-    if (x, y+2) not in visited and (x,y+2) in grid:
-        neighb.append("S")
-        if (x, y+2) not in queue:
-            queue.append((x,y+2))
+    # Tworzymy przestrzeń w której ma powstać labirynt
+    grid = {(x, y): 1 for x in range(1, s + 1) for y in range(1, s + 1)}
 
-#Funkcje Draw* rysują nam ścieżkę
-def DrawLeft():
-    pygame.draw.rect(screen, white, ((x+1)*20-20,y*20-20,20,20))
-    pygame.display.update()
-    time.sleep(.001)
-    
-def DrawRight():
-    pygame.draw.rect(screen, white, ((x-1)*20-20,y*20-20,20,20))
-    pygame.display.update()
-    time.sleep(.001)
+    def CheckNeighbours():  # Funkcja sprawdza ile mamy wolnych sąsiednich kratek
+        if (x + 2, y) not in visited and (x + 2, y) in grid:
+            neighb.append("E")
+            if (x + 2, y) not in queue:
+                queue.append((x + 2, y))
 
-def DrawDown():
-    pygame.draw.rect(screen, white, (x*20-20,(y-1)*20-20,20,20))
-    pygame.display.update()
-    time.sleep(.001)
+        if (x, y + 2) not in visited and (x, y + 2) in grid:
+            neighb.append("S")
+            if (x, y + 2) not in queue:
+                queue.append((x, y + 2))
 
-def DrawUp():
-    pygame.draw.rect(screen, white, (x*20-20,(y+1)*20-20,20,20))
-    pygame.display.update()
-    time.sleep(.001)
+    # Funkcje Draw* rysują nam ścieżkę
 
-def DrawPosition():
-    pygame.draw.rect(screen, red, (x*20-20,y*20-20,20,20))
-    pygame.display.update()
-    time.sleep(.03)
+    def DrawRight():
+        pygame.draw.rect(screen, white, ((x - 1) * 20 - 20, y * 20 - 20, 20, 20))
+        pygame.display.update()
+        time.sleep(.001)
 
-    pygame.draw.rect(screen, white, (x*20-20,y*20-20,20,20))
-    pygame.display.update()
-    time.sleep(.001)
+    def DrawDown():
+        pygame.draw.rect(screen, white, (x * 20 - 20, (y - 1) * 20 - 20, 20, 20))
+        pygame.display.update()
+        time.sleep(.001)
 
-#Ustawiamy parametry dla pierwszej komórki
-DrawPosition()
-neighb = []
-CheckNeighbours()
+    def DrawPosition():
+        pygame.draw.rect(screen, red, (x * 20 - 20, y * 20 - 20, 20, 20))
+        pygame.display.update()
+        time.sleep(.03)
 
-#Rozpoczynamy główną pętle
-exit = False
-while not exit:
-    while len(queue) > 0:
+        pygame.draw.rect(screen, white, (x * 20 - 20, y * 20 - 20, 20, 20))
+        pygame.display.update()
+        time.sleep(.001)
 
-        if len(neighb) > 0: #Dla sprawdzonych "sąsiadów" wybieramy losowo jeden wolny, tworzymy ścieżkę oraz zaznaczamy jako odwiedzony
-            random_neighb = (random.choice(neighb))
-            
-            queue.append((x,y))
+    # Ustawiamy parametry dla pierwszej komórki
+    DrawPosition()
+    neighb = []
+    CheckNeighbours()
 
-            if random_neighb == "E":
-                x = x + 2
-                DrawRight()
+    # Rozpoczynamy główną pętle
+    exit = False
+    while not exit:
+        while len(queue) > 0:
 
-            if random_neighb == "S":
-                y = y + 2
-                DrawDown()
+            if len(
+                    neighb) > 0:  # Dla sprawdzonych "sąsiadów" wybieramy losowo jeden wolny, tworzymy ścieżkę oraz zaznaczamy jako odwiedzony
+                random_neighb = (random.choice(neighb))
 
-            x, y = queue.pop()
+                queue.append((x, y))
 
-        x, y = queue.pop(0) #Przechodzimy do pierwszej pozycji w naszej kolejce
-        
-        #Zaznaczamy pozycję oraz oznaczamy ją jako odwiedzoną
-        DrawPosition()
-        visited.append((x,y))
+                if random_neighb == "E":
+                    x = x + 2
+                    DrawRight()
 
-        neighb = []
-        CheckNeighbours() #Sprawdzamy dla danej pozycji wolnych "sąsiadów"
+                if random_neighb == "S":
+                    y = y + 2
+                    DrawDown()
 
-        for event in pygame.event.get(): #Pętla for pozwala nam opuścić program w dowolnym momencie
+                x, y = queue.pop()
+
+            x, y = queue.pop(0)  # Przechodzimy do pierwszej pozycji w naszej kolejce
+
+            # Zaznaczamy pozycję oraz oznaczamy ją jako odwiedzoną
+            DrawPosition()
+            visited.append((x, y))
+
+            neighb = []
+            CheckNeighbours()  # Sprawdzamy dla danej pozycji wolnych "sąsiadów"
+
+            for event in pygame.event.get():  # Pętla for pozwala nam opuścić program w dowolnym momencie
+                if event.type == pygame.QUIT:
+                    exit = True
+
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit = True
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit = True
+    pygame.quit()
 
-pygame.quit()
+# generate()
--- join.py
+++ join.py
@@ -1,6 +1,7 @@
 import labirynth_generator as bk_lab
 import PySimpleGUI as sg
-
+import PyMazeDFS
+import PyMazeBFS
 sg.theme('Topanga')
 
 
@@ -22,10 +23,50 @@ def bartek():
     window.close()
 
 
+def konrad_dfs():
+    layout = [[sg.Text("Labirynt generwany algorytmem DFS", justification='center', font='Helvetica 15')],
+              [sg.Text('Podaj wymiar N labiryntu:', justification='center', font='Helvetica 15'), sg.InputText(size=(8, 5),font='Helvetica 18')],
+              [sg.Button("Generuj labirynt NxN", size=(15, 1), font='Helvetica 18')],
+              [sg.Button("Zamknij okno", size=(10, 1), font='Helvetica 18')]
+              ]
+    # Create the Window
+    window = sg.Window('Generator labiryntów', layout, size=(500, 200), element_justification='c')
+    # Event Loop to process "events"
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event in (None, 'Zamknij okno'):
+            break
+        if event in (None, 'Generuj labirynt NxN'):
+            PyMazeDFS.generate(int(values[0]))
+    window.close()
+
+
+def konrad_bfs():
+    layout = [[sg.Text("Labirynt generwany algorytmem BFS", justification='center', font='Helvetica 15')],
+              [sg.Text('Podaj wymiar N labiryntu:', justification='center', font='Helvetica 15'), sg.InputText(size=(8, 5),font='Helvetica 18')],
+              [sg.Button("Generuj labirynt NxN", size=(15, 1), font='Helvetica 18')],
+              [sg.Button("Zamknij okno", size=(10, 1), font='Helvetica 18')]
+              ]
+    # Create the Window
+    window = sg.Window('Generator labiryntów', layout, size=(500, 200), element_justification='c')
+    # Event Loop to process "events"
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event in (None, 'Zamknij okno') or None:
+            break
+        if event in (None, 'Generuj labirynt NxN'):
+            PyMazeBFS.generate(int(values[0]))
+    window.close()
+
+
 layout = [
     [sg.Text("Witaj! Wybierz jeden z poniższych generatorów labiryntów.", justification='center', font='Helvetica 15')],
     [sg.Text("Naciśnięcie jednego z poniższych przycisków otworzy nowe okno konfiguracji", justification='center',
              font='Helvetica 15')],
+    [sg.Button('DFS', size=(15, 1), font='Helvetica 18')],
+    [sg.Button('BFS', size=(15, 1), font='Helvetica 18')],
     [sg.Button('Prosty BFS bez argumentów', size=(15, 1), font='Helvetica 18')],
     [sg.Button("Zakończ program", size=(10, 1), font='Helvetica 18')]
 ]
@@ -35,8 +76,12 @@ window = sg.Window('Wybór generatora', layout, size=(550, 280), element_justifi
 while True:
     event, values = window.read()
     print(event, values)
-    if event in (None, 'Zakończ program'):
+    if event in (None, 'Zakończ program') or None:
         break
     if event in (None, 'Prosty BFS bez argumentów'):
         bartek()
+    if event in (None, 'DFS'):
+        konrad_dfs()
+    if event in (None, 'BFS'):
+        konrad_bfs()
 window.close()
